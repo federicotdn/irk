0435c86314a07976df3a7c4adbe7193ee3964197
Federico Tedin <federicotedin@gmail.com>
2026-01-02 01:42:27 +0100

    New test

--- test/IrkSpec.hs
+++ test/IrkSpec.hs
@@ -4,77 +4,47 @@ import qualified Data.Map as Map
 import Data.Maybe (fromJust)
 import Irk
 import Language (languages)
+import System.OsPath (OsPath)
 import Test.Hspec
-import Testing (asPosix, getTestTextFileSize)
-import Types (IrkFile (..), IrkFileArea (..))
-import Utils (os, oss)
+import Testing (asPosix)
+import Types (IrkFile (..))
+import Utils (os)
 
--- Helper to normalize paths in results for cross-platform comparison
-normalizePaths :: [[IrkFile]] -> [[IrkFile]]
-normalizePaths = map (map (\f -> f {iPath = asPosix (iPath f)}))
+getPaths :: [[IrkFile]] -> [[OsPath]]
+getPaths = map (map (asPosix . iPath))
 
 spec :: Spec
 spec = do
   describe "searchPaths" $ do
     let py = fromJust $ Map.lookup "python" languages
+    let go = fromJust $ Map.lookup "go" languages
 
     it "retrieves paths correctly (case 1)" $ do
-      testPySize <- Just <$> getTestTextFileSize "python/test.py"
-      vendoredPySize <- Just <$> getTestTextFileSize "python/.venv/vendored.py"
-
       let searches = searchPaths py Nothing [os "test/data/python"]
       allPaths <- sequence searches
-      normalizePaths allPaths
+      getPaths allPaths
         `shouldBe` [ [],
-                     [ IrkFile
-                         (os "test/data/python/test.py")
-                         [os "test.py"]
-                         False
-                         testPySize
-                         1
-                         Workspace
-                     ],
-                     [ IrkFile
-                         (os "test/data/python/.venv/vendored.py")
-                         (oss [".venv", "vendored.py"])
-                         False
-                         vendoredPySize
-                         2
-                         WorkspaceVendored
-                     ],
+                     [os "test/data/python/test.py"],
+                     [os "test/data/python/.venv/lib/vendored.py"],
                      []
                    ]
 
     it "retrieves paths correctly (case 2)" $ do
-      testPySize <- Just <$> getTestTextFileSize "python/test.py"
-      vendoredPySize <- Just <$> getTestTextFileSize "python/.venv/vendored.py"
-
       let searches = searchPaths py (Just (os "test/data/python/foo.py")) [os "test/data/python"]
       allPaths <- sequence searches
-      normalizePaths allPaths
-        `shouldBe` [ [ IrkFile
-                         (os "test/data/python/foo.py")
-                         []
-                         False
-                         Nothing
-                         0
-                         Workspace
-                     ],
-                     [ IrkFile
-                         (os "test/data/python/test.py")
-                         [os "test.py"]
-                         False
-                         testPySize
-                         1
-                         Workspace
-                     ],
-                     [ IrkFile
-                         (os "test/data/python/.venv/vendored.py")
-                         (oss [".venv", "vendored.py"])
-                         False
-                         vendoredPySize
-                         2
-                         WorkspaceVendored
-                     ],
+      getPaths allPaths
+        `shouldBe` [ [os "test/data/python/foo.py"],
+                     [os "test/data/python/test.py"],
+                     [os "test/data/python/.venv/lib/vendored.py"],
+                     []
+                   ]
+
+    it "retrieves paths correctly (case 3)" $ do
+      let searches = searchPaths go Nothing [os "test/data/go"]
+      allPaths <- sequence searches
+      getPaths allPaths
+        `shouldBe` [ [],
+                     [os "test/data/go/main.go"],
+                     [os "test/data/go/vendor/vendored.go"],
                      []
                    ]
